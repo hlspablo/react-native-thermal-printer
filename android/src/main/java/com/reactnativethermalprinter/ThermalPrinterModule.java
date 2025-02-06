69f88e45ceead8b0199537607a049073dc83ba23
Ferenc Farkas <ferenc.farkas@happygastro.hu>
2025-02-06 21:06:25 +0100

    asd

--- android/src/main/java/com/reactnativethermalprinter/ThermalPrinterModule.java
+++ android/src/main/java/com/reactnativethermalprinter/ThermalPrinterModule.java
@@ -64,7 +64,7 @@ public class ThermalPrinterModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void printTcp(String ipAddress, double port, String payload, boolean autoCut, boolean openCashbox, double mmFeedPaper, double printerDpi, double printerWidthMM, double printerNbrCharactersPerLine, double timeout, String charset, double encodingId, Promise promise) {
+  public void printTcp(String ipAddress, double port, String payload, boolean autoCut, boolean openCashbox, double mmFeedPaper, double printerDpi, double printerWidthMM, double printerNbrCharactersPerLine, double timeout, String charset, int encodingId, Promise promise) {
 //
 //        05-05-2021
 //        https://reactnative.dev/docs/native-modules-android
@@ -85,7 +85,7 @@ public class ThermalPrinterModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void printBluetooth(String macAddress, String payload, boolean autoCut, boolean openCashbox, double mmFeedPaper, double printerDpi, double printerWidthMM, double printerNbrCharactersPerLine, String charset, double encodingId, Promise promise) {
+  public void printBluetooth(String macAddress, String payload, boolean autoCut, boolean openCashbox, double mmFeedPaper, double printerDpi, double printerWidthMM, double printerNbrCharactersPerLine, String charset, int encodingId, Promise promise) {
     this.jsPromise = promise;
     BluetoothConnection btPrinter;
 
